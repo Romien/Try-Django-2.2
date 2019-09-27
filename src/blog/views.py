a31b6b28a704d6b905533b1ce9ca1f0e351e10ce
Roman Makarchyk <messarro@gmail.com>
2019-09-27 09:24:10 +0300

    Finish Video 36

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,17 +1,60 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .forms import BlogPostModelForm
 from .models import BlogPost
 
 
-def blog_post_detail_page(request, post_id):
-    try:
-        obj = BlogPost.objects.get(id=post_id)
-    except BlogPost.DoesNotExist:
-        raise Http404
-    except ValueError:
-        raise Http404
-    template_name = 'blog_post_detail.html'
+# CRUD
+
+# GET -> Retrieve / List
+
+# POST -> Create / Update / DELETE
+
+# Create Retrieve Update Delete
+
+
+def blog_post_list_view(request):
+    # list out objects
+    # could be search
+    qs = BlogPost.objects.all()  # queryset -> list of python object
+    template_name = 'blog/list.html'
+    context = {'object_list': qs}
+    return render(request, template_name, context)
+
+
+def blog_post_create_view(request):
+    # create objects
+    # ? use a form
+    form = BlogPostModelForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        print(form.cleaned_data)
+        form = BlogPostModelForm()
+    template_name = 'form.html'
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
+def blog_post_detail_view(request, slug):
+    # 1 object -> detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/detail.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
+
+
+def blog_post_update_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/update.html'
+    context = {"object": obj, 'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_delete_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/delete.html'
     context = {"object": obj}
     return render(request, template_name, context)
--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
+
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -13,4 +16,19 @@ def about_page(request):
 
 
 def contact_page(request):
-    return render(request, "hello_world.html", {"title": "Contact Us"})
+    # print(request.POST)
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    context = {
+        "title": "Contact Us",
+        "form": form}
+    return render(request, "form.html", context)
+
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
